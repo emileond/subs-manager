12691e05026652c75cc204b6ff89cdf680be928b
Emilio <sonarart@gmail.com>
2020-10-08 20:31:23 -0500

    styles fixes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './sass/styles.scss'
 import {router} from './router/index.routes'
 
-console.log(window.location.hash)
 router(window.location.hash)
 
 window.addEventListener('hashchange', () => {
